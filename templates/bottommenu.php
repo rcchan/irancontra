11e5dbb715dee7a68b5e43f991f852845d9366e9
Ryan Chan <ryan@rcchan.com>
2017-11-15 03:18:34 -0500

    Remove press gallery.

--- templates/bottommenu.php
+++ templates/bottommenu.php
@@ -3,8 +3,6 @@
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="multimedia.php" class="menu_link">MEDIA</a>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="media.php" class="menu_link">PRESS</a>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="profiles.php" class="menu_link">PROFILES</a>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href="timelines.php" class="menu_link">TIMELINES</a>
