328ce2d7ed6cc92660e4a911fb82c6b098593635
Ryan Chan <ryan@rcchan.com>
2018-10-18 21:07:59 -0700

    jp-gallery divs

--- h-jp-gallery.php
+++ h-jp-gallery.php
@@ -42,84 +42,96 @@
 <h3>JOHN POINDEXTER (9) - <a href="profile-poindexter.php" class="content_link">Profile</a></h3>
 <p>Lt. Col. Oliver North was a tough act to follow, but Adm. John Poindexter provided plenty of engaging testimony.&nbsp; Poindexter had resigned as national security advisor after the affair came to light, having served in one of the top two positions in Reagan’s National Security Council for over three years.&nbsp; Contrary to North, Poindexter wore civilian clothing, a deliberate decision he made because “this issue is not a Navy issue,” he said on the first day of hearings.&nbsp; (He also periodically came across as a relaxed witness by smoking a pipe.)&nbsp; Appearance aside, Poindexter gave substantial—and blunt—testimony from the start.&nbsp; He admitted that he tore up the Reagan’s signed Finding that sent arms to Iran in order to avoid “political embarrassment.”&nbsp; Poindexter only discussed the Diversion with North, intentionally keeping Reagan in the dark even though he believed Reagan would have approved of it.&nbsp; Contrary to his predecessor, Robert McFarlane, Poindexter insisted that the Boland Amendment did not apply to the NSC.&nbsp; He testified that the “buck” stopped with him, but he still claimed to “not recall” several key incidents.&nbsp; When Sen. Paul Trible (R-VA) questioned Poindexter’s adherence to the Naval Honor Code, Poindexter defended himself by declaring, “I don't have any regrets for anything that I did.”</p>
 <div align="center">
-  <table border="0"><tr><td valign="top">
-    <table width="150"><tr><td valign="top"><a href="transcripts/jp-1.php"><img src="thumbnails/jp1.jpg" style="border-color:#000" border="1"/></a></td></tr>
-      <tr>
-        <td align="left"><font class="video"><strong><a href="transcripts/jp-1.php">Poindexter on Destroying the Finding</a> </strong><br />
-          (13:31)</font></td></tr></table>
-    </td><td valign="top">
-      <table width="150"><tr><td valign="top"><a href="transcripts/jp-2.php"><img src="thumbnails/jp2.jpg" style="border-color:#000" border="1"/></a></td></tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-2.php">Poindexter on Reagan's Knowledge of Selling Arms to Iran</a> </strong><br />
-            (6:17)</font></td></tr></table>
-      </td><td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/jp-3.php"><img src="thumbnails/jp3.jpg" style="border-color:#000" border="1"/></a></td>
-          </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-3.php">Poindexter on Keeping Reagan's Unaware</a></strong><br />
-            (7:35)</font></td>
-          </tr>
-        </table></td></tr>
-    <tr>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/jp-4.php"><img src="thumbnails/jp4.jpg" style="border-color:#000" border="1"/></a></td>
-          </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-4.php">Poindexter on Boland's Application to NSC</a></strong><br />
-            (3:34)</font></td>
-          </tr>
-        </table></td>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/jp-5.php"><img src="thumbnails/jp5.jpg" style="border-color:#000" border="1"/></a></td>
-        </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-5.php">Poindexter on What Reagan Approved</a></strong><br />
-            (3:53)</font></td>
-        </tr>
-      </table></td>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/jp-6.php"><img src="thumbnails/jp6.jpg" style="border-color:#000" border="1"/></a></td>
-        </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-6.php">Poindexter on reagan's Knowledge of North's Activities</a></strong><br />
-            (2:25)</font></td>
-        </tr>
-      </table></td>
-      </tr>
-    <tr>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/jp-7.php"><img src="thumbnails/jp7.jpg" style="border-color:#000" border="1"/></a></td>
-        </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-7.php">Poindexter &quot;Not Recall[ing]&quot; Anything</a></strong><br />
-            (7:06)</font></td>
-        </tr>
-      </table></td>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/jp-8.php"><img src="thumbnails/jp8.jpg" style="border-color:#000" border="1"/></a></td>
-        </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-8.php">Sen. Trible Asking Poindexter About the Naval Honor Code </a></strong><br />
-            (1:39)</font></td>
-        </tr>
-      </table></td>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/jp-9.php"><img src="thumbnails/jp9.jpg" style="border-color:#000" border="1"/></a></td>
-        </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/jp-9.php">Rep. Hamilton on Where the &quot;Buck Stops&quot;</a></strong><br />
-            (2:56)</font></td>
-        </tr>
-      </table></td>
-    </tr>
-  </table>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-1.php"><img src="thumbnails/jp1.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-1.php" class="video">Poindexter on Destroying the Finding</a>
+    <br />
+    (13:31)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-2.php"><img src="thumbnails/jp2.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-2.php" class="video">Poindexter on Reagan's Knowledge of Selling Arms to Iran</a>
+    <br />
+    (6:17)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-3.php"><img src="thumbnails/jp3.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-3.php" class="video">Poindexter on Keeping Reagan's Unaware</a>
+    <br />
+    (7:35)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-4.php"><img src="thumbnails/jp4.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-4.php" class="video">Poindexter on Boland's Application to NSC</a>
+    <br />
+    (3:34)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-5.php"><img src="thumbnails/jp5.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-5.php" class="video">Poindexter on What Reagan Approved</a>
+    <br />
+    (3:53)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-6.php"><img src="thumbnails/jp6.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-6.php" class="video">Poindexter on reagan's Knowledge of North's Activities</a>
+    <br />
+    (2:25)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-7.php"><img src="thumbnails/jp7.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-7.php" class="video">Poindexter "Not Recall[ing]" Anything</a>
+    <br />
+    (7:06)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-8.php"><img src="thumbnails/jp8.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-8.php" class="video">Sen. Trible Asking Poindexter About the Naval Honor Code </a>
+    <br />
+    (1:39)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/jp-9.php"><img src="thumbnails/jp9.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/jp-9.php" class="video">Rep. Hamilton on Where the "Buck Stops"</a>
+    <br />
+    (2:56)
+  </div>
+</div>
 </div></div>
    </td></tr></table><br /><div align=center class="top_link"><p>[ <a href="#headertop">Return to top </a>]</p></div><br /><br />
 </div>
