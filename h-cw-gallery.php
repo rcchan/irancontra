1b64ecadeb51f54a912796e006d043b5d26c69e2
Ryan Chan <ryan@rcchan.com>
2018-10-18 21:10:06 -0700

    cw-gallery divs

--- h-cw-gallery.php
+++ h-cw-gallery.php
@@ -42,47 +42,56 @@
 <h3>CASPAR WEINBERGER (5) - <a href="profile-weinberger.php" class="content_link">Profile</a></h3>
 <p>The final public witness in the Iran-Contra hearings was Secretary of Defense Caspar Weinberger, whose views on Iran policy did not win out despite his protests, he testified.&nbsp; He claimed that he was opposed to the administration’s Iran policy and that reaching out to Iran “was just contrary to everything” he knew.&nbsp; Still, Weinberger’s advice had been trumped by those of Adm. John Poindexter of the National Security Council and the late CIA head William Casey, he said.&nbsp; The secretary of defense testified that he knew of no Iranian official who were moderate or “not virulently anti-American.”&nbsp; Despite his objection, the arms-for-hostages deal went forward.&nbsp; The secretary of defense blamed “people with their own agenda” for preventing the president from hearing the best strategy to deal with Iran.&nbsp; Weinberger testified that he did not recall the president stating that Iran was losing the war, but he would have stepped in to correct Reagan in such a case.&nbsp; “One is not shy about presenting views that seem to me would be in opposition to what I thought was something that was clearly wrong,” he said.</p>
 <div align="center">
-  <table border="0"><tr><td valign="top">
-    <table width="150"><tr><td valign="top"><a href="transcripts/cw-1.php"><img src="thumbnails/cw1.jpg" style="border-color:#000" border="1"/></a></td></tr>
-      <tr>
-        <td align="left"><font class="video"><strong><a href="transcripts/cw-1.php">Weinberger on Who Was Winning the Iran-Iraq War</a> </strong><br />
-          (2:49)</font></td></tr></table>
-    </td><td valign="top">
-      <table width="150"><tr><td valign="top"><a href="transcripts/cw-2.php"><img src="thumbnails/cw2.jpg" style="border-color:#000" border="1"/></a></td></tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/cw-2.php">Rep. Aspin Questions Weinberger on Not Dealing with Iranian Moderates</a> </strong><br />
-            (5:38)</font></td></tr></table>
-      </td><td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/cw-3.php"><img src="thumbnails/cw3.jpg" style="border-color:#000" border="1"/></a></td>
-          </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/cw-3.php">Rep. Broomfield Questions Weinberger on Iranian Relationship</a></strong><br />
-            (4:03)</font></td>
-          </tr>
-        </table></td></tr>
-    <tr>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/cw-4.php"><img src="thumbnails/cw4.jpg" style="border-color:#000" border="1"/></a></td>
-          </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/cw-4.php">Weinberger Tells Sen. Sarbanes About &quot;People With Their Own Agenda&quot;</a></strong><br />
-            (2:41)</font></td>
-          </tr>
-        </table></td>
-      <td valign="top"><table width="150">
-        <tr>
-          <td valign="top"><a href="transcripts/cw-5.php"><img src="thumbnails/cw5.jpg" style="border-color:#000" border="1"/></a></td>
-        </tr>
-        <tr>
-          <td align="left"><font class="video"><strong><a href="transcripts/cw-5.php">Sen. Nunn Questions Weinberger on Lack of Cabinet Communication</a></strong><br />
-            (4:45)</font></td>
-        </tr>
-      </table></td>
-      <td valign="top">&nbsp;</td>
-      </tr>
-  </table>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/cw-1.php"><img src="thumbnails/cw1.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/cw-1.php" class="video">Weinberger on Who Was Winning the Iran-Iraq War</a>
+    <br />
+    (2:49)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/cw-2.php"><img src="thumbnails/cw2.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/cw-2.php" class="video">Rep. Aspin Questions Weinberger on Not Dealing with Iranian Moderates</a>
+    <br />
+    (5:38)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/cw-3.php"><img src="thumbnails/cw3.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/cw-3.php" class="video">Rep. Broomfield Questions Weinberger on Iranian Relationship</a>
+    <br />
+    (4:03)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/cw-4.php"><img src="thumbnails/cw4.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/cw-4.php" class="video">Weinberger Tells Sen. Sarbanes About "People With Their Own Agenda"</a>
+    <br />
+    (2:41)
+  </div>
+</div>
+<div class="galleryvideocontainer">
+  <div class="galleryvideothumbnail">
+    <a href="transcripts/cw-5.php"><img src="thumbnails/cw5.jpg" /></a>
+  </div>
+  <div class="galleryvideolink">
+    <a href="transcripts/cw-5.php" class="video">Sen. Nunn Questions Weinberger on Lack of Cabinet Communication</a>
+    <br />
+    (4:45)
+  </div>
+</div>
 </div></div>
    </td></tr></table><br /><div align=center class="top_link"><p>[ <a href="#headertop">Return to top</a> ]</p></div><br /><br />
 </div>
