70a9ed2f0d1f8f2e5e5dfc04f1e5b8b34586f696
Ryan Chan <ryan@rcchan.com>
2018-10-23 03:08:39 -0400

    Dynamically generate gallery

--- gallery.php
+++ gallery.php
@@ -1,3 +1,25 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.12/handlebars.min.js"></script>
+<script src="gallery.js"></script>
+<script id="gallery-template" type="text/x-handlebars-template">
+  {{#each videos}}
+    <div class="galleryvideocontainer">
+      <div class="galleryvideothumbnail">
+        <a href="v-{{key}}.php"><img src="thumbnails/{{key}}.jpg" /></a>
+      </div>
+      <div class="galleryvideolink">
+        <a href="v-{{key}}.php" class="video">{{title}}</a>
+        <br />
+        ({{duration duration}})
+      </div>
+    </div>
+  {{/each}}
+</script>
+<script>
+var pad = s => s < 10 ? 0 + s : s;
+Handlebars.registerHelper("duration", d => Math.floor(d / 60) + ':' + pad(d % 60));
+document.write(Handlebars.compile(document.getElementById('gallery-template').innerHTML)({videos: videos}));
+</script>
+<!--
 <div class="galleryvideocontainer">
   <div class="galleryvideothumbnail">
     <a href="v-os1.php"><img src="thumbnails/os1.jpg" /></a>
@@ -888,3 +910,4 @@
     (10:01)
   </div>
 </div>
+-->
