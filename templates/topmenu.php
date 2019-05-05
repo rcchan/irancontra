c1961f03e0ccb9799858c396b41ad66427ab5444
Ryan Chan <ryan@rcchan.com>
2019-05-05 17:20:21 -0400

    Update search behavior

--- templates/topmenu.php
+++ templates/topmenu.php
@@ -4,32 +4,17 @@
 &nbsp&nbsp&nbsp&nbsp<a href="links.php" class="menu_link">LINKS</a>
 </div>
 <div style="float: right; width: 250px;">
-<div id="cse-search-form">Loading</div>
-<script src="//www.google.com/jsapi" type="text/javascript"></script>
-<script type="text/javascript"> 
-  google.load('search', '1', {language : 'en', style : google.loader.themes.ESPRESSO});
-  google.setOnLoadCallback(function() {
-    var customSearchOptions = {};
-    var orderByOptions = {};
-    orderByOptions['keys'] = [{label: 'Relevance', key: ''},{label: 'Date', key: 'date'}];
-    customSearchOptions['enableOrderBy'] = true;
-    customSearchOptions['orderByOptions'] = orderByOptions;  var customSearchControl = new google.search.CustomSearchControl(
-      '004598588032668485318:t_pjll3haje', customSearchOptions);
-    customSearchControl.setResultSetSize(google.search.Search.LARGE_RESULTSET);
-    var options = new google.search.DrawOptions();
-    options.enableSearchboxOnly("http://www.google.com/cse?cx=004598588032668485318:t_pjll3haje");
-    customSearchControl.draw('cse-search-form', options);
-  }, true);
+<script>
+  (function() {
+    var cx = '004598588032668485318:t_pjll3haje';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
 </script>
-
-<style type="text/css">
-  input.gsc-input {
-    border-color: #D3BCA1;
-  }
-  input.gsc-search-button {
-    border-color: #300D00;
-    background-color: #461200;
-  }</style>
-                                
+<gcse:searchbox-only></gcse:searchbox-only>                  
 </div>
 </div>
