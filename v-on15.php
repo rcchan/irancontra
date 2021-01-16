14fe539a90c98a741309225f26b3fbb46f07911a
Ryan Chan <ryan@rcchan.com>
2021-01-16 00:25:35 -0500

    Update video in v-on15.php

--- v-on15.php
+++ v-on15.php
@@ -29,7 +29,7 @@ Day 25: July 9, 1987
 </td>
 <td align="justify" valign="top">
 
-<object id='cspan-video-player' classid='clsid:d27cdb6eae6d-11cf-96b8-444553540000' codebase='http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0' align='middle' height='500' width='410'><param name='allowScriptAccess' value='true'/><param name='movie' value='http://www.c-spanvideo.org/videoLibrary/assets/swf/CSPANPlayer.swf?pid=9534-1&start=16153&end=16326'/><param name='quality' value='high'/><param name='bgcolor' value='#ffffff'/><param name='allowFullScreen' value='true'/><param name='flashvars' value='system=http://www.c-spanvideo.org/common/services/flashXml.php?programid=144247&style=full&start=16153&end=16326'/><embed name='cspan-video-player' src='http://www.c-spanvideo.org/videoLibrary/assets/swf/CSPANPlayer.swf?pid=9534-1&start=16153&end=16326' base='http://www.c-spanvideo.org/videoLibrary/assets/swf/' allowScriptAccess='always' bgcolor='#ffffff' quality='high' allowFullScreen='true' type='application/x-shockwave-flash' pluginspage='http://www.macromedia.com/go/getflashplayer' flashvars='system=http://www.c-spanvideo.org/common/services/flashXml.php?programid=144247&style=full&start=16153&end=16326' align='middle' height='500' width='410'></embed></object>
+<video controls style="max-width: 100%;"><source src="http://www.brown.edu/Research/Understanding_the_Iran_Contra_Affair/videos/cut/program.144247.MP4-M20.16149.177.mp4" type="video/mp4"></video>
 
    </td></tr></table><br /><br />
 </div>
