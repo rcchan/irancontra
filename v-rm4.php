5a9013f99d68af29bd023da180e3e5c98124dd25
mdortenziojr <34410098+mdortenziojr@users.noreply.github.com>
2021-09-02 18:15:35 -0400

    v-rm4
    Added hyphen

--- v-rm4.php
+++ v-rm4.php
@@ -2,22 +2,22 @@
 <title>Understanding the Iran-Contra Affairs</title>
 </head>
 <? require ('templates/hearingsbanner.php'); ?>
-    
+
 <div style="margin: 10px 0px 0px 20px; width: 760px" align="justify" id="welcomenote">
     <div align="left" class="bread_crumbs"><a href="index.php">Home</a> &raquo; <a href="thehearings.php">The Hearings</a> &raquo; <a href="gallery.php">Video Gallery</a> &raquo; McFarlane on Supposed "Oil-Drilling Equipment" (Hawk Missiles)</div>
 <table>
 <tr>
 <td colspan=2 height=75><h2>McFarlane on Supposed "Oil-Drilling Equipment" (Hawk Missiles)</h2>
 <h3>Day 5: May 11, 1987</h3></td></tr>
-<td colspan=2><p>National Security Advisor McFarlane admits that he, Adm. Poindexter, and Col. North created a misleading chronology stating the United States was purchasing oil drilling equipment, when really they were purchasing Hawk missiles.</p></td></tr>
+<td colspan=2><p>National Security Advisor McFarlane admits that he, Adm. Poindexter, and Col. North created a misleading chronology stating the United States was purchasing oil-drilling equipment, when really they were purchasing Hawk missiles.</p></td></tr>
 <tr><td align="left">&laquo; <a href="v-rm3.php" class="content_link">Previous</a></td><td align="right"><a href="v-rm5.php" class="content_link">Next</a> &raquo;</td></tr>
 <tr><td colspan=2>
 <br /><strong>TRANSCRIPT:</strong></td></tr>
 <tr>
 <td width=350 valign="top">
 
-<iframe src="transcripts/rm-4.php" style="border:none" width="350" height="500"></iframe>	
-    
+<iframe src="transcripts/rm-4.php" style="border:none" width="350" height="500"></iframe>
+
 </td>
 <td align="justify" valign="top">
 <video controls style="max-width: 100%;"><source src="http://www.brown.edu/Research/Understanding_the_Iran_Contra_Affair/videos/cut/program.5571.MP4-M20.3057.191.mp4" type="video/mp4"></video>
